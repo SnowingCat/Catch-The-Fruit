a0e8490ded3c05c356542636112ce175c4e69031
toohard99 <109780569+SnowingCat@users.noreply.github.com>
2024-03-14 18:30:40 +0800

    语言转换固定在左下角，同时增添了一些新功能。
    至少把图片转换写了出来，还固定了位置

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,9 @@
+var flag = true;
+
+
 function repository() {
     window.open("https://github.com/SnowingCat/Catch-The-Fruit",'target');
 }
-
 
 function start() {
 
@@ -13,4 +15,16 @@ function mode() {
 
 function introduction() {
     alert("主界面都还没开发完呢")
+}
+
+function change_language() {
+    alert("转换语言在开发中...");
+    var img = document.getElementById("lg");
+    if(flag){
+        img.src = "../img/language/Chinese.png";
+        flag = false;
+    }else{
+        img.src = "../img/language/English.png";
+        flag = true;
+    }
 }
