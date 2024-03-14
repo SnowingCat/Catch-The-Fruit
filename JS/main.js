8e1ac33864fd88a92b5f8fad2f8687f3608f25f6
toohard99 <109780569+SnowingCat@users.noreply.github.com>
2024-03-15 01:47:33 +0800

    修改了主页
    将按钮设置为蓝底白字，并添加了setting“设置”按钮

--- JS/main.js
+++ JS/main.js
@@ -23,12 +23,14 @@ function change_language() {
     var mode = document.getElementById("btn_mode");
     var repository = document.getElementById("btn_repository");
     var introduction = document.getElementById("btn_introduction");
+    var setting = document.getElementById("btn_setting");
     if(flag){
         img.src = "../img/language/Chinese.png";
         start.innerText = "开始游戏";
         mode.innerText = "模式选择";
         repository.innerText = "项目地址";
         introduction.innerText = "游戏介绍";
+        setting.innerText = "游戏设置";
         flag = false;
     }else{
         img.src = "../img/language/English.png";
@@ -36,6 +38,11 @@ function change_language() {
         mode.innerText = "mode";
         repository.innerText = "repository";
         introduction.innerText = "introduction";
+        setting.innerText = "游戏设置";
         flag = true;
     }
+}
+
+function setting() {
+    alert("还在开发中...")
 }
