955f50a0e8b3cef8b587228d50263daace50fc60
toohard99 <109780569+SnowingCat@users.noreply.github.com>
2024-03-15 18:52:59 +0800

    添加了很多东西
    start写了一部分，主页面终于用灰色背景覆盖了，暂时添加了一个用不着的篮子图片元素，之后的慢慢来。

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,20 @@
-var flag = true;
+var flag1 = true; //控制中英文转换的变量
 
 
 function repository() {
-    window.open("https://github.com/SnowingCat/Catch-The-Fruit",'target');
+    window.open("https://github.com/SnowingCat/Catch-The-Fruit",'target');//跳转至个人仓库
 }
 
 function start() {
-
+    var start_menu = document.getElementById("start_menu");
+    var title = document.getElementById("title");
+    var buttons = document.getElementById("buttons");
+    var lg = document.getElementById("lg");//获取开始界面元素
+    start_menu.style.display = "none";
+    title.style.display = "none";
+    buttons.style.display = "none";
+    lg.style.display = "none";//隐藏开始界面元素
+    alert("游戏正在开发中...")
 }
 
 function mode() {
@@ -17,29 +25,29 @@ function introduction() {
     alert("主界面都还没开发完呢")
 }
 
-function change_language() {
+function change_language() {//中英文转换
     var img = document.getElementById("lg");
     var start = document.getElementById("btn_start");
     var mode = document.getElementById("btn_mode");
     var repository = document.getElementById("btn_repository");
     var introduction = document.getElementById("btn_introduction");
-    var setting = document.getElementById("btn_setting");
-    if(flag){
+    var setting = document.getElementById("btn_setting"); //获取所有需要转换的元素
+    if(flag1){//切换中文
         img.src = "../img/language/Chinese.png";
         start.innerText = "开始游戏";
         mode.innerText = "模式选择";
         repository.innerText = "项目地址";
         introduction.innerText = "游戏介绍";
         setting.innerText = "游戏设置";
-        flag = false;
-    }else{
+        flag1 = false;
+    }else{//切换英文
         img.src = "../img/language/English.png";
         start.innerText = "start";
         mode.innerText = "mode";
         repository.innerText = "repository";
         introduction.innerText = "introduction";
         setting.innerText = "setting";
-        flag = true;
+        flag1 = true;
     }
 }
 
