f6a8e94de918c98e6fbbdd9b68e0f0f5dd1844ac
toohard99 <109780569+SnowingCat@users.noreply.github.com>
2024-03-18 17:57:32 +0800

    加了更多元素
    1.introduction开始编写
    2.img补充了一些图片
    3.introduction页面开始编写
    #流程图不用去管它，用于辅助理解的

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,5 @@
 var flag1 = true; //控制中英文转换的变量
 
-
 function repository() {
     window.open("https://github.com/SnowingCat/Catch-The-Fruit",'target');//跳转至个人仓库
 }
@@ -22,7 +21,16 @@ function mode() {
 }
 
 function introduction() {
-    alert("主界面都还没开发完呢")
+    var start_menu = document.getElementById("start_menu");
+    var title = document.getElementById("title");
+    var buttons = document.getElementById("buttons");
+    var lg = document.getElementById("lg");
+    var h1 = document.getElementById("introduction");
+    start_menu.style.display = "none";
+    title.style.display = "none";
+    buttons.style.display = "none";
+    lg.style.display = "none";//隐藏开始界面元素
+    h1.style.display = "block";
 }
 
 function change_language() {//中英文转换
@@ -31,7 +39,9 @@ function change_language() {//中英文转换
     var mode = document.getElementById("btn_mode");
     var repository = document.getElementById("btn_repository");
     var introduction = document.getElementById("btn_introduction");
-    var setting = document.getElementById("btn_setting"); //获取所有需要转换的元素
+    var setting = document.getElementById("btn_setting"); 
+    var h1 = document.getElementById("introduction")
+    var back = document.getElementById("btn-back")//获取所有的元素
     if(flag1){//切换中文
         img.src = "../img/language/Chinese.png";
         start.innerText = "开始游戏";
@@ -39,6 +49,8 @@ function change_language() {//中英文转换
         repository.innerText = "项目地址";
         introduction.innerText = "游戏介绍";
         setting.innerText = "游戏设置";
+        h1.innerText = "游戏介绍";
+        back.innerText = "返回";
         flag1 = false;
     }else{//切换英文
         img.src = "../img/language/English.png";
@@ -47,6 +59,8 @@ function change_language() {//中英文转换
         repository.innerText = "repository";
         introduction.innerText = "introduction";
         setting.innerText = "setting";
+        h1.innerText = "Introduction";
+        back.innerText = "back";
         flag1 = true;
     }
 }
